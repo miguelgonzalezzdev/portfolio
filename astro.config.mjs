13d22fc11562dd13553bc822b1b05b151cf45e11
miguelgonzalezzdev <miguelgonzalezdev@gmail.com>
2025-08-08 21:06:34 +0200

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://miguelgonzalezzdev.github.io',
+  base: 'portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
