f19b8ff78fb51c4f396f35d7a284b5cc7ad95290
miguelgonzalezzdev <miguelgonzalezdev@gmail.com>
2025-11-17 12:41:58 +0100

    Solventar error ruta imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://miguelgonzalezzdev.github.io',
   base: '/portfolio',
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
   },
 });
